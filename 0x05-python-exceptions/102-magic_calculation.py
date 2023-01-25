f17271e4a917c8ee14113ca138a50bc3b7bb408b
Bruke <BrukeD12@gmail.com>
2023-01-25 14:19:30 -0800

    Error fix

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
                 continue
             else:
                 result += (a ** b) / i
-        except Invalid Calculation:
+        except Exception:
             result = b + a
             break
         finally:
